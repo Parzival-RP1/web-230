ca34f749830745eba77ac49bef15ccdd884bbb78
Daniel Coleman <dcoleman.dev@gmail.com>
2017-12-03 15:44:52 -0600

    Adding coleman/assignent.js

---
+++ week-1/Coleman_Assignment1.4.js
@@ -0,0 +1,26 @@
+var firstName = "Daniel";
+var lastName = "Coleman";
+var age = "31";
+var street = "Soto";
+var city = "Martinez";
+var state = "California";
+var zipCode = "94553";
+console.log('\n');
+
+var firstName;
+var lastName;
+var age;
+var street;
+var city;
+var state;
+var zipCode;
+
+firstName = "daniel";
+lastName = "coleman";
+age = "31";
+street = "soto";
+city = "martinez";
+state = "california";
+zipCode = "94553";
+
+console.log('\n');
