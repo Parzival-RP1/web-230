7b6a5b34ee81696349f6e4b960f5497c91b8c363
Daniel Coleman <dcoleman.dev@gmail.com>
2017-12-10 17:33:37 -0600

    Uploading header.js && Coleman_Assignment_2.3.js

---
+++ header.js
@@ -0,0 +1,9 @@
+var myFirstName = "Daniel";
+var myLastName = "Coleman";
+var todaysDate = new Date().toLocaleDateString();
+var assignmentNum = "Assignment 2.3";
+
+var programHeader = "\n" + myFirstName + " " + myLastName + "\n" +
+    assignmentNum + "\nDate: " + todaysDate;
+
+console.log(programHeader);
---
+++ week-2/Coleman_Assignment_2.3.js
@@ -0,0 +1,35 @@
+var header = require('../header.js');
+
+// start program
+
+/*
+    Expected output:
+
+    Daniel Coleman
+    Exercise 2.3
+    12/10/17
+
+    Hello Daniel Coleman!
+
+    Hint: Use your personal header display function and
+    refer to page 178 for implementing function properties
+*/
+
+// function properties go here...
+
+myName.danielColeman
+var myName = "Daniel Coleman"
+
+// function 
+function myName() {
+
+}
+
+// output 
+console.log('\nHello ' + myName + '!');
+
+
+
+
+
+// end program
