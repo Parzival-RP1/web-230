bb02b99f39081bdf6331ffd5082dbba001939626
Daniel Coleman <dcoleman.dev@gmail.com>
2017-12-10 17:54:02 -0600

    Update header.js && add Coleman_Assignment_2.2.js

--- header.js
+++ header.js
@@ -1,9 +1,25 @@
-var myFirstName = "Daniel";
-var myLastName = "Coleman";
-var todaysDate = new Date().toLocaleDateString();
-var assignmentNum = "Assignment 2.3";
+/*
+============================================
+; Title:  Program Header
+; Author: Professor Krasso 
+; Date:   25 June 2017
+; Modified By: Daniel Coleman
+; Description: Displays a formatted header
+;===========================================
+*/
 
-var programHeader = "\n" + myFirstName + " " + myLastName + "\n" +
-    assignmentNum + "\nDate: " + todaysDate;
+/*
+    This function accepts four parameters: 
+        f = students first name
+        l = students last name 
+        a = assignment number
+    and displays the results.
+*/
 
-console.log(programHeader);
+module.exports = {
+    display: function(f, l, a) {
+        var msg = "\n" + f + " " + l + "\n" +
+            a + "\nDate: " + new Date().toLocaleDateString();
+        return msg;
+    }
+};
