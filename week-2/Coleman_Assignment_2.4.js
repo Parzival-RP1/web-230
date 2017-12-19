52fe10f1e83a900c0a0c18282288736bf96191e3
Daniel Coleman <32473171+Parzival-RP1@users.noreply.github.com>
2017-12-19 14:53:41 -0600

    Code updates across all sections - Function updates to body

--- week-2/Coleman_Assignment_2.4.js
+++ week-2/Coleman_Assignment_2.4.js
@@ -45,18 +45,19 @@ function fullName(firstName, lastName) {
 }
 
 //Create a function called dateWriter that accepts three parameters (year, month, and day) as arguments and returns a fully qualified JavaScript date object
-function dateWriter() {
+function dateWriter(16, 11, 2017) {
     return dateSelect;
 }
 
-var today = new Date();
+var today = new Date(day, month, year);
 var dateSelect = today.toDateString();
 
-var day = today.setDate(16);
-var month = today.setMonth(11);
-var year = today.setFullYear(2017);
+/*
+var day = today.setDate();
+var month = today.setMonth();
+var year = today.setFullYear();
 
-/* SUGGESTED KRASSO CODE
+SUGGESTED KRASSO CODE
 function dateWriter(year, month, day) {
     return new Date(year, month, day);
 }
@@ -64,31 +65,25 @@ console.log(dateWriter(2017, 12, 18));
 */
 
 //Create a function called formatNumber that accepts two parameters (number and fixed position) as arguments and returns the formatted value
-function formatNumber() {
+function formatNumber(45.859) {
     return temp;
 }
+var temp = temp.toFixed(1);
 
-var far = 45.859;
-var temp = far.toFixed(1);
-var tempFar = formatNumber();
 
 //Create a function called convertToInt that accepts one parameter (string) as an argument and returns the value as a parsed integer
-function convertToInt() {
-    return parseInt(31)
+function convertToInt(31) {
+    return parseInt()
 }
-
-var age = convertToInt();
 
 //Create a function called convertToFloat that accepts one parameter (string) as an argument and returns the value as a parsed float
-function convertToFloat() {
-    return parseFloat(1000000000.00)
+function convertToFloat(1000000000.00) {
+    return parseFloat()
 }
-
-var savings = convertToFloat();
 
 // output 
 console.log(header.display("Daniel", "Coleman", "Assignment 2.4"));
 console.log('\nHello my name is ' + fullName("Daniel", "Coleman"));
-console.log("\nToday's date is " + today.toLocaleDateString() + " and the current temperature is " + formatNumber() + '.');
+console.log("\nToday's date is " + dateWriter() + " and the current temperature is " + formatNumber() + '.');
 console.log("\nI am " + convertToInt() + " years old and my savings account goal is " + convertToFloat() + '.');
 // end program
