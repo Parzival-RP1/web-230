0cd7b140197992ff364391096a5d49fba083876f
Daniel Coleman <dcoleman.dev@gmail.com>
2017-12-20 11:54:51 -0600

    Update to 2.4 code // functions & console.log reoganization

--- week-2/Coleman_Assignment_2.4.js
+++ week-2/Coleman_Assignment_2.4.js
@@ -1,20 +1,27 @@
 var header = require('../header.js');
 
+
+/* ============================================ 
+; Title: Assignment 2.4
+; Author: Daniel Coleman
+; Date: 19 December 2017
+; Modified By: Daniel Coleman
+; Description: Creating and calling functions with multiple paramaters. Returning strings, parsed integers, and formatted values
+;=========================================== */
+
+
+
 // start program
 
 /*
     Expected output:
-
     FirstName LastName
     Assignment 2.4
     Today's Date
-
     Hello my name is <concatenated full name>!
-
     Today's date is <formatted date> and the current temperature is <formatted number with 1 fixed decimal position>
     
     I am <parsed int> years old and my savings account goal is <parsed float value> dollars.
-
 */
 
 
@@ -22,73 +29,49 @@ var header = require('../header.js');
 // function(s) go here...
 
 
-//Full Name Block
-var firstName = "Daniel ";
-var lastName = "Coleman";
-var Name = fullName();
-
-
-function fullName() {
-    return firstName + lastName;
+function fullName(firstName, lastName) {
+    return "Hello my name is " + firstName + " " + lastName + "!";
 }
-
-fullName()
-
-//Create a function called dateWriter that accepts three parameters (year, month, and day) as arguments and returns a fully qualified JavaScript date object
-function dateWriter() {
-    return dateSelect;
-}
-
-var today = new Date();
-var dateSelect = today.toDateString();
-
-var day = today.setDate(16);
-var month = today.setMonth(11);
-var year = today.setFullYear(2017);
-
-dateWriter()
-
-/*
-function dateWriter(year, month, day) {
-    return new Date(year, month, day);
-}
-console.log(dateWriter(2017, 12, 18));
-*/
 
 //Create a function called formatNumber that accepts two parameters (number and fixed position) as arguments and returns the formatted value
-function formatNumber() {
-    return temp;
+function formatNumber(temp, position) {
+    return temp.toFixed(position);
 }
 
-var far = 45.859;
-var temp = far.toFixed(1);
-var tempFar = formatNumber();
+//Create a function called dateWriter that accepts three parameters (year, month, and day) as arguments and returns a fully qualified JavaScript date object
+function dateWriter(month, day, year) {
+    return "Today's date is " + month + "/" + day + "/" + year + " and the current temperature is " + formatNumber(45.859, 1) + ".";
+}
 
-formatNumber()
 
 
 //Create a function called convertToInt that accepts one parameter (string) as an argument and returns the value as a parsed integer
-function convertToInt() {
-    return parseInt(31)
+function convertToInt(age) {
+    var convertInt = Number(age);
+    return "I am " + convertInt + " years old and my savings account goal is $" + convertToFloat(10000000) + ".";
+
 }
-
-var age = convertToInt();
-
-convertToInt()
 
 //Create a function called convertToFloat that accepts one parameter (string) as an argument and returns the value as a parsed float
-function convertToFloat() {
-    return parseFloat(1000000000.00)
+function convertToFloat(numberToConvert) {
+    return parseFloat(numberToConvert)
 }
 
-var savings = convertToFloat();
-
-convertToFloat()
-
-
-// output 
+/* output OLD
 console.log(header.display("Daniel", "Coleman", "Assignment 2.4"));
-console.log('\nHello my name is ' + fullName() + '!');
-console.log("\nToday's date is " + today.toLocaleDateString() + " and the current temperature is " + formatNumber() + '.');
-console.log("\nI am " + convertToInt() + " years old and my savings account goal is " + convertToFloat() + '.');
+console.log('\nHello my name is ' + fullName("Daniel", "Coleman"));
+console.log("\nToday's date is " + dateWriter(19, 11, 2017) + " and the current temperature is " + formatNumber(45.859, 1) + '.');
+console.log("\nI am " + convertToInt(31) + " years old and my savings account goal is " + convertToFloat(1000000000.00) + '.');
+*/
+
+// output
+
+console.log(header.display("Daniel", "Coleman", "Assignment 2.4"));
+console.log('\n');
+console.log(fullName("Daniel", "Coleman"));
+console.log('\n');
+console.log(dateWriter(12, 20, 2017));
+console.log('\n');
+console.log(convertToInt("31"));
+
 // end program
