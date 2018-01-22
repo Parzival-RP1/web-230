2864d874378f64ccc76cdc37d1b15cf05f5fe179
Daniel Coleman <dcoleman.dev@gmail.com>
2018-01-21 21:01:13 -0600

    Upload Assignment 6.4

--- week-6/coleman-assignment-6.4.js
+++ week-6/coleman-assignment-6.4.js
@@ -10,5 +10,39 @@ console.log("\n");
 ; Author: Richard Krasso / Daniel Coleman
 ; Date: 20 January 2018
 ; Modified By: Daniel Coleman
-; Description: Nested Object Literals
+; Description: Create an object literal ticket with a nested object literal person
 ;=========================================== */
+
+/*
+ Expected output:
+
+ FirstName LastName
+ Assignment 6.4
+ Today's Date
+
+ Ticket <id> was created on <dateCreated> and assigned to employee <firstName lastName> (<jobTitle>).
+
+ */
+
+// start program
+
+var ticket = {
+    // ticket properties
+    id: "789",
+    name: "Kenna Lombar",
+    dateCreated: "January 21, 2018",
+    priority: "High",
+    personId: "KLomb",
+
+    person: {
+        // person properties
+        id: "654",
+        firstName: "Ash",
+        lastName: "Ketchum",
+        jobTitle: "Pokemon Master",
+    }
+};
+
+console.log("Ticket " + ticket.id + " was created on " + ticket.dateCreated + " and assigned to employee " + ticket.person.firstName + " " + ticket.person.lastName + " (" + ticket.person.jobTitle + ").");
+
+// end program
